5145a006e35fc0fd35c2216300e358a0fd6d02b8
lutm6666 <lutm8888@gmail.com>
2026-09-19 13:30:47 +0800

    Bump Case 2 canon engine cache for opening narrative

--- case2-engine.js
+++ case2-engine.js
@@ -125,8 +125,8 @@ function loadRainRuntime(done){
 
  function loadEngine(){
   if(window.Case2RainCanon){flushLoaded();return}
-  loadScript('case2-rain-canon-engine.js?v=6',function(ok){
-   if(!ok||!window.Case2RainCanon){reportLoadError('case2-rain-canon-engine.js?v=6');return}
+  loadScript('case2-rain-canon-engine.js?v=7',function(ok){
+   if(!ok||!window.Case2RainCanon){reportLoadError('case2-rain-canon-engine.js?v=7');return}
    flushLoaded();
   });
  }
